548d8a82eedb0e04f096c0ce2c5fb5bf43783dda
Emre <emre@guclu.io>
2024-03-02 09:48:05 +0300

    updated python

--- prices.py
+++ prices.py
@@ -1,22 +1,29 @@
 import urllib.request as request
 import json
-import time
+
 def get_virtual_machine_sku_prices(region):
-    skus = list(json['skus'].keys())
+    skus = list(jsondata['skus'].keys())
+    
+    found_price_count=0
     for sku in skus:
-        pricing_types = json['skus'][sku].keys()
-        for pricing_type in pricing_types:
-            offer_info = json['skus'][sku][pricing_type][0].split('--')
+        sku_info = sku.split('-')
+            offer_info = jsondata['skus'][sku][pricing_type][0].split('--')
             offer_sku = offer_info[0]
             offer_type = offer_info[1]
-            price = json['offers'][offer_sku]['prices'][offer_type][region]['value']
-            print(f"sku: {sku}, pricing_type: {pricing_type}, offer_type: {offer_sku}, offer_type: {offer_type}, price: {price}")
+            try :
+                price = jsondata['offers'][offer_sku]['prices'][offer_type][region]['value']
+                found_price_count += 1
+                print(f"sku: {sku}, pricing_type: {pricing_type}, offer_type: {offer_sku}, offer_type: {offer_type}, price: {price}")
+            except KeyError:
+                print(f"Price information not available for sku: {sku}, pricing_type: {pricing_type}, in region: {region}")
+    print(f"Number of skus: {len(skus)}")
+    print(f"Number of prices found for region {region}: {found_price_count}")
 
 virtual_machine_calculator_api = 'https://azure.microsoft.com/api/v3/pricing/virtual-machines/calculator/?culture=en-us&currency=$currency'
 response=request.urlopen(virtual_machine_calculator_api)
 data = response.read()
-json = json.loads(data)
-regions  = json['regions']
-for region in regions:
-    print(region['slug'])
+jsondata = json.loads(data)
+regions  = jsondata['regions']
 
+
+get_virtual_machine_sku_prices('us-east')
